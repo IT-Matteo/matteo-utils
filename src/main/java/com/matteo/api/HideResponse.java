1ddd35e6635e0e7d1ad92be2ca30956a4f3f2786
matteo <18262392301@163.com>
2018-05-20 18:09:21 +0800

    del target

---
+++ src/main/java/com/matteo/api/HideResponse.java
@@ -0,0 +1,28 @@
+package com.matteo.api;
+
+import java.lang.annotation.*;
+
+/**
+ * *****************************************************************************
+ * <p>
+ * 功能名           ：com.matteo.api
+ * 系统名           ：
+ * <p>
+ * *****************************************************************************
+ * Modification History
+ * <p>
+ * Date        Name                    Reason for Change
+ * ----------  ----------------------  -----------------------------------------
+ * 2018/5/20      matteo                 Created
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface HideResponse {
+    /**
+     * 作用
+     *
+     * @return
+     */
+    String value() default "隐藏响应的日志";
+}
